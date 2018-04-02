105b2c5696c744647e15b284a088b745188b121e
kiva <cao86457119@163.com>
2018-04-02 11:02:15 +0800

    Reset Project

---
+++ src/main/java/cn/com/ceri/sctest/sctest/SctestApplication.java
@@ -0,0 +1,12 @@
+package cn.com.ceri.sctest.sctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SctestApplication.class, args);
+	}
+}
